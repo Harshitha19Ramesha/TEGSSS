5716dc7a62ce76c71fa8f6212d7e386c09a25c1a
Harshitha19Ramesha <harshitha19.ramesh@gmail.com>
2025-03-13 15:31:20 +0530

    first commit

---
+++ TEGSSS/src/main/java/com/Harshitha19Ramesha/App.java
@@ -0,0 +1,13 @@
+package com.Harshitha19Ramesha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
